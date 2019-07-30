13d1e4d51a594b0d37bbb8b7ba3eb8a0e50194d4
Evan Stoll <evanjsx@gmail.com>
2019-07-30 09:20:47 -0400

    Add shell.nix

--- shell.nix
+++ shell.nix
@@ -4,7 +4,8 @@
     name = "rust-env";
     buildInputs = [
         # Additional Dependencies
-        rustup pkgconfig openssl stdenv bashInteractive xorg.libX11 xorg.libXrandr feh
+        # nightly-rust-with-extensions -> https://github.com/evanjs/nixos_cfg/blob/ed183e9f25524fba0828b9317ad807c88cfa85b6/overlays/evanjs/pkgs/nightly-rust/default.nix
+        nightly-rust-with-extensions.base pkgconfig openssl stdenv bashInteractive xorg.libX11 xorg.libXrandr SDL2 feh
     ];
 
     # Environment Variables
